8e4387e87304ac6e98f6e1becf0b31bd06b75716
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-10-22 13:59:23 +0200

    Start ES7 and Kibana in Docker-Compose

--- install-es7.sh
+++ install-es7.sh
@@ -1,4 +1,6 @@
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.4.0
+# docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.4.0
+
+docker-compose up -d kibana
 
 wget http://media.sundog-soft.com/es7/shakes-mapping.json
 
