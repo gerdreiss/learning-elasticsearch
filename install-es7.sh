0b642615251d9ff9320a9753c645fa7a671f0594
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-10-22 11:45:42 +0200

    Section 1/4 Installing Elasticsearch

---
+++ install-es7.sh
@@ -0,0 +1,12 @@
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.4.0
+
+wget http://media.sundog-soft.com/es7/shakes-mapping.json
+
+curl -H "Content-Type: application/json" -XPUT localhost:9200/shakespeare --data-binary @shakes-mapping.json
+
+wget http://media.sundog-soft.com/es7/shakespeare_7.0.json
+
+curl -H "Content-Type: application/json" -XPOST localhost:9200/shakespeare/_bulk --data-binary @shakespeare_7.0.json
+
+curl -H "Content-Type: application/json" -XGET localhost:9200/shakespeare/_search?pretty -d '{ "query": { "match_phrase": { "text_entry": "to be or not to be" }}}'
+
