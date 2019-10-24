23e0e16e084ca083f3bf103b2bbb57069376d9c9
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-10-24 10:32:06 +0200

    Section 2 concurrency

---
+++ Section_02/concurrent-reqs.sh
@@ -0,0 +1,30 @@
+curl -XGET localhost:9200/movies/_doc/109487?pretty
+# {
+#   "_index" : "movies",
+#   "_type" : "_doc",
+#   "_id" : "109487",
+#   "_version" : 6,
+#   "_seq_no" : 9,
+#   "_primary_term" : 1,
+#   "found" : true,
+#   "_source" : {
+#     "genre" : [
+#       "Action",
+#       "Sci-Fi",
+#       "IMAX"
+#     ],
+#     "title" : "Interstellar",
+#     "year" : 2014
+#   }
+# }
+
+curl -H "Content-Type: application/json" -XPUT "localhost:9200/movies/_doc/109487?if_seq_no=9&if_primary_term=1" -d @interstellar-update.json
+
+# Update document via POST .../_update
+curl -H "Content-Type: application/json" -XPOST localhost:9200/movies/_doc/109487/_update?retry_on_conflict=5 -d '
+{
+    "doc": {
+        "genre": ["Sci-Fi", "Action", "IMAX"]
+    }
+}
+'
