9df01bac2cfe9b2947b0cfbc775c83ccd2aa6049
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-10-28 14:11:37 +0100

    Section 4 mysql

--- Section_04/logstash.sh
+++ Section_04/logstash.sh
@@ -1,5 +1,7 @@
-wget -P downloads media.sundog-soft.com/es/access_log
+wget -P downloads http://media.sundog-soft.com/es/access_log
+wget -P downloads http://files.grouplens.org/datasets/movielens/ml-100k.zip
 
 curl -H "Content-Type: application/json" -XGET "localhost:9200/_cat/indices?v"
 
 curl -H "Content-Type: application/json" -XGET "localhost:9200/logstash-2019.10.28-000001/_search?pretty"
+
